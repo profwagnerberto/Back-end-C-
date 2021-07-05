01845fedfebc5e8a625c4ba47569b9e2eef73edd
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2021-07-05 16:44:14 +0000

    Subindo formatos

--- main.cs
+++ main.cs
@@ -1,7 +1,18 @@
 using System;
+using System.IO;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+    Conta c = new Conta();
+    c.numero = 1;
+    c.titular = "victor";
+    c.saldo = 100;
+
+    StreamWriter sw = new StreamWriter("dados/Conta.dat",true);
+    sw.WriteLine("{0,5},{1,-20},{2,9:F2},", 
+                  c.numero, c.titular, c.saldo);
+    sw.Close();
+
   }
 }
---
+++ modelo/Conta.cs
@@ -0,0 +1,7 @@
+class Conta
+{
+  // numero, titular e saldo são atributos do objeto
+  public int numero;
+  public string titular;
+  public double saldo;
+}
